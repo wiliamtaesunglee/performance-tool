4638e1f6a5e1ccd78d187327808b95c2a8a08d1a
wil.i.am <wiliam.tae@gmail.com>
2020-01-10 08:35:18 -0300

    layout component added and index.hmtl cleared on public folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import { Header } from './components/layout/Header';
+import { Sidebar } from './components/layout/Sidebar';
+import { Content } from './components/layout/Content';
 
 export const App =() => (
   <div className="App">
     <Header/>
+    <Sidebar/>
+    <Content/>
     todo list + pomodoro + chat
   </div>
 );
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Sidebar = () => (
+  <div>i'm a sidebar</div>
+)
